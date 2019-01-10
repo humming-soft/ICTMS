8ecdaaa8e075c2508406154a579e03f433c932eb
unknown <Ancy@HS-PC07.Office.Hummingsoft.com.my>
2019-01-10 09:22:45 +0800

    Signed-off-by: unknown <Ancy@HS-PC07.Office.Hummingsoft.com.my>

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -6,13 +6,19 @@ class Dashboard extends ICTMS_Controller {
     function __construct()
     {
         parent::__construct();
+        $this->load->model('dashboardModel','',TRUE);
     }
 	public function index()
 	{
+        $this->load->helper('form');
         $session_data = $this->session->userdata('logged_in');
         $data['username'] =$data1['username'] = $session_data['username'];
         $role_id = $session_data['roleid'];
-
+        $data['location']=$this->dashboardModel->select_location();
+        $data['category']=$this->dashboardModel->select_category();
+        $data['sector']=$this->dashboardModel->select_sector();
+        
+        $data['thrust']=$this->dashboardModel->select_thrust();
         if($this->session->userdata('message'))
         {
             $messagehrecord=$this->session->userdata('message');
@@ -28,7 +34,21 @@ class Dashboard extends ICTMS_Controller {
         $this->load->view('core/fragments/header',$_header);
         $this->load->view('core/fragments/main_navbar',$data1);
         $this->load->view('core/fragments/secondary_navbar');
-        $this->load->view('core/dashboard');
+        $this->load->view('core/dashboard',$data);
         $this->load->view('core/fragments/footer');
+    }
+    public function get_subsector()
+    {
+        $sectorId = $this->input->post();
+        $data=$this->dashboardModel->select_sub_sector($sectorId);
+        echo json_encode($data);
+
+    }
+    public function get_focusarea()
+    {
+        $thrustId = $this->input->post();
+        $data=$this->dashboardModel->select_focusarea($thrustId);
+        echo json_encode($data);
+
     }
 }
---
+++ application/models/dashboardModel.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+Class dashboardModel extends CI_Model
+{
+    function select_location()
+    {
+        $sql = "SELECT pjt_location_id, pjt_location_name FROM \"pjt-location\"";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+    function select_category()
+    {
+        $sql = "SELECT pjt_category_id, pjt_category_name FROM \"pjt-category\"";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+    function select_sector()
+    {
+        $sql = "SELECT pjt_sector_id, pjt_sector_name FROM \"pjt-sector\"";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+    function select_sub_sector($sectorId)
+    {
+        $values=$sectorId['subSector'];
+        $sql = "SELECT pjt_sub_sector_id, pjt_sector_id, pjt_sub_sector_name FROM \"pjt-sub-sector\" where pjt_sector_id = $values";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+    function select_thrust()
+    {
+        $sql = "SELECT thrust_id, thrust FROM thrust";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+    function select_focusarea($thrustId)
+    {
+        $values = $thrustId['thrust'];
+        $sql = "SELECT focus_area_id, thrust_id, focus_area FROM \"focus-area\" where thrust_id = $values";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+}
